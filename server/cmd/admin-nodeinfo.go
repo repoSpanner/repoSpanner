68d8c1354537759ebfe301ccdf198867341aff15
Patrick Uiterwijk <patrick@puiterwijk.org>
2018-10-24 17:47:30 +0200

    Add --nagios nodestatus
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- server/cmd/admin-nodeinfo.go
+++ server/cmd/admin-nodeinfo.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -14,6 +15,27 @@ var adminNodeInfoCmd = &cobra.Command{
 	Long:  `Print information about a node.`,
 	Run:   runAdminNodeStatus,
 	Args:  cobra.ExactArgs(0),
+}
+
+func outputRawStatus(resp datastructures.NodeStatus, timesinceping time.Duration) {
+	fmt.Printf("Node ID: %d\n", resp.NodeID)
+	fmt.Printf("Node name: %s\n", resp.NodeName)
+	fmt.Printf("Time since last ping: %s\n", timesinceping.Truncate(time.Second))
+}
+
+func outputNagiosStatus(resp datastructures.NodeStatus, timesinceping time.Duration) {
+	msg := fmt.Sprintf("Node %s has last pinged %s ago", resp.NodeName, timesinceping.Truncate(time.Second))
+
+	if timesinceping >= (15 * time.Second) {
+		fmt.Println("CRITICAL:", msg)
+		os.Exit(2)
+	} else if timesinceping >= (5 * time.Second) {
+		fmt.Println("WARNING:", msg)
+		os.Exit(2)
+	} else {
+		fmt.Println("OK:", msg)
+		os.Exit(0)
+	}
 }
 
 func runAdminNodeStatus(cmd *cobra.Command, args []string) {
@@ -32,12 +54,15 @@ func runAdminNodeStatus(cmd *cobra.Command, args []string) {
 	lastselfpingtime := time.Unix(0, *lastselfping.Timestamp)
 	timesinceping := time.Since(lastselfpingtime)
 
-	fmt.Printf("Node ID: %d\n", resp.NodeID)
-	fmt.Printf("Node name: %s\n", resp.NodeName)
-	fmt.Printf("Time since last ping: %s\n", timesinceping.Truncate(time.Second))
-
+	if val, _ := cmd.Flags().GetBool("nagios"); val {
+		outputNagiosStatus(resp, timesinceping)
+	} else {
+		outputRawStatus(resp, timesinceping)
+	}
 }
 
 func init() {
 	adminCmd.AddCommand(adminNodeInfoCmd)
+
+	adminNodeInfoCmd.Flags().Bool("nagios", false, "Return nagios-formatted output")
 }
