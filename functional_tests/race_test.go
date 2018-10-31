21ace5edfa583de70d43db61eee216492ea83452
Patrick Uiterwijk <patrick@puiterwijk.org>
2018-10-31 12:48:05 +0100

    Refactor race test to remove duplicated code
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- functional_tests/race_test.go
+++ functional_tests/race_test.go
@@ -9,8 +9,8 @@ import (
 	"testing"
 )
 
-const numRaceRepos = 10
-const numRaceBranches = 20
+const numRaceRepos = 5
+const numRaceBranches = 10
 const numRaceCommits = 50
 
 type fail struct {
@@ -65,164 +65,133 @@ func TestStress(t *testing.T) {
 
 	tester := createGoroutineTester(t)
 
-	raceCreateRepos(tester, nodes)
-	tester.resolveFails()
-	racePull(tester, nodes, "pre")
-	tester.resolveFails()
-	raceCreateRepoContents(tester)
-	tester.resolveFails()
-	racePush(tester, nodes)
-	tester.resolveFails()
-	racePull(tester, nodes, "post")
-	tester.resolveFails()
-	raceVerifyRepos(tester)
-	tester.resolveFails()
+	// Preparation done, start stress tests
+
+	// 1. Create repositories
+	runRace(tester, nodes, false, "Creating repositories", raceCreateRepo)
+	// 2. First (empty) pull
+	racePullRepos(tester, nodes, "master", false)
+	// 3. Create master branches
+	runRace(tester, nodes, false, "Preparing master", racePrepareMaster)
+	// 4. Push master branches
+	racePushRepos(tester, nodes, "master", false)
+	// 5. Second pull set
+	racePullRepos(tester, nodes, "prepare", true)
+	// 6. Fill branches with test contents
+	runRace(tester, nodes, true, "Preparing branch contents", racePrepareBranchContents)
+	// 7. Push branches
+	racePushRepos(tester, nodes, "prepare", true)
+	// 8. Third pull set
+	racePullRepos(tester, nodes, "confirm", true)
+	// 9. Verify branch contents
+	runRace(tester, nodes, true, "Verifying contents", raceVerifyRepo)
 }
 
 func nthNode(nodes []nodeNrType, nr, shift int) nodeNrType {
 	return nodes[(shift+nr)%len(nodes)]
 }
 
-func raceCreateRepos(t tester, nodes []nodeNrType) {
-	var createWg sync.WaitGroup
-	syncchan := make(chan interface{})
-	t.Logf("Creating repos")
-	for i := 0; i <= numRaceRepos; i++ {
-		reponame := fmt.Sprintf("race%d", i)
-		createWg.Add(1)
-		go func(i int, rname string) {
-			defer createWg.Done()
-			node := nthNode(nodes, i, 0)
-
-			// This channel is used to make all goroutines schedulable at the same time
-			<-syncchan
-
-			t.Logf("Creating repo %s at node %d", reponame, node)
-			createRepo(t, node, rname, false)
-		}(i, reponame)
-	}
-	t.Logf("Starting repo creation")
-	close(syncchan)
-	t.Logf("Waiting for repo creations to finish")
-	createWg.Wait()
-	t.Logf("Repos created")
+func raceCreateRepo(t tester, nodes []nodeNrType, repo, branch int) {
+	reponame := fmt.Sprintf("race%d", repo)
+	node := nthNode(nodes, repo, 0)
+	createRepo(t, node, reponame, false)
 }
 
-func raceVerifyRepos(t tester) {
-	var checkWg sync.WaitGroup
+func raceVerifyRepo(t tester, nodes []nodeNrType, repo, branch int) {
+	repodir := raceCloneDir("confirm", true, repo, branch)
 
-	for repo := 0; repo < numRaceRepos; repo++ {
-		checkWg.Add(1)
-
-		go func(repo int) {
-			defer checkWg.Done()
-
-			reldir := fmt.Sprintf("race_%d_post", repo)
-			repodir := path.Join(cloneDir, reldir)
-
-			for branch := 0; branch < numRaceBranches; branch++ {
-				branchname := fmt.Sprintf("branch_%d_%d", repo, branch)
-				runRawCommand(t, "git", repodir, nil, "checkout", branchname)
-				testFiles(t, repodir, 0, numRaceCommits+10-1)
-			}
-		}(repo)
-	}
-	t.Logf("Waiting for checks")
-	checkWg.Wait()
-	t.Logf("Repos all checked")
+	branchname := fmt.Sprintf("branch_%d_%d", repo, branch)
+	runRawCommand(t, "git", repodir, nil, "checkout", branchname)
+	testFiles(t, repodir, 0, numRaceCommits+10-1)
 }
 
-func raceCreateRepoContents(t tester) {
-	var contentsWg sync.WaitGroup
-
-	t.Logf("Creating repo contents")
-	for repo := 0; repo < numRaceRepos; repo++ {
-		contentsWg.Add(1)
-
-		go func(repo int) {
-			defer contentsWg.Done()
-
-			reldir := fmt.Sprintf("race_%d_pre", repo)
-			repodir := path.Join(cloneDir, reldir)
-
-			// Make sure "master" exists
-			writeTestFiles(t, repodir, 0, 3)
-			runRawCommand(t, "git", repodir, nil, "commit", "-sm", "Writing our tests")
-
-			for branch := 0; branch < numRaceBranches; branch++ {
-				branchname := fmt.Sprintf("branch_%d_%d", repo, branch)
-				runRawCommand(t, "git", repodir, nil, "branch", branchname, "master")
-				runRawCommand(t, "git", repodir, nil, "checkout", branchname)
-
-				for commit := 0; commit < numRaceCommits; commit++ {
-					writeTestFiles(t, repodir, 1, commit+10)
-					runRawCommand(
-						t, "git", repodir, nil, "commit", "-sm",
-						fmt.Sprintf("Test repo %d, branch %d, commit %d", repo, branch, commit),
-					)
-				}
-			}
-		}(repo)
-	}
-	t.Logf("Waiting for content generation")
-	contentsWg.Wait()
-	t.Logf("Contents all generated")
+func racePrepareMaster(t tester, nodes []nodeNrType, repo, branch int) {
+	repodir := raceCloneDir("master", false, repo, branch)
+	writeTestFiles(t, repodir, 0, 3)
+	runRawCommand(t, "git", repodir, nil, "commit", "-sm", "Writing our tests")
 }
 
-func racePush(t tester, nodes []nodeNrType) {
-	var pushWg sync.WaitGroup
-	syncchan := make(chan interface{})
-	t.Logf("Initializing push")
-	for repo := 0; repo < numRaceRepos; repo++ {
-		for branch := 0; branch < numRaceBranches; branch++ {
-			pushWg.Add(1)
-			go func(repo, branch int) {
-				defer pushWg.Done()
+func racePrepareBranchContents(t tester, nodes []nodeNrType, repo, branch int) {
+	repodir := raceCloneDir("prepare", true, repo, branch)
+	branchname := fmt.Sprintf("branch_%d_%d", repo, branch)
+	runRawCommand(t, "git", repodir, nil, "branch", branchname, "master")
+	runRawCommand(t, "git", repodir, nil, "checkout", branchname)
+	for commit := 0; commit < numRaceCommits; commit++ {
+		writeTestFiles(t, repodir, 1, commit+10)
+		runRawCommand(
+			t, "git", repodir, nil, "commit", "-sm",
+			fmt.Sprintf("Test repo %d, branch %d, commit %d", repo, branch, commit),
+		)
+	}
+}
 
-				reldir := fmt.Sprintf("race_%d_pre", repo)
-				repodir := path.Join(cloneDir, reldir)
+func raceCloneDir(suffix string, forBranches bool, repo, branch int) string {
+	reldir := fmt.Sprintf("race_%d_%s", repo, suffix)
+	if forBranches {
+		reldir += fmt.Sprintf("_%d", branch)
+	}
+	return path.Join(cloneDir, reldir)
+}
+
+func racePushRepos(t *goRoutineTester, nodes []nodeNrType, suffix string, forBranches bool) {
+	runRace(
+		t,
+		nodes,
+		forBranches,
+		"Pushing: "+suffix,
+		func(t tester, nodes []nodeNrType, repo, branch int) {
+			repodir := raceCloneDir(suffix, forBranches, repo, branch)
+			if forBranches {
 				branchname := fmt.Sprintf("branch_%d_%d", repo, branch)
-
-				<-syncchan
-
-				t.Logf("Pushing branch %s for repo %d", branchname, repo)
-
-				if branch == 0 {
-					runRawCommand(t, "git", repodir, nil, "push", "origin", "master")
-				}
 				runRawCommand(t, "git", repodir, nil, "push", "origin", branchname)
-			}(repo, branch)
+			} else {
+				runRawCommand(t, "git", repodir, nil, "push", "origin", "master")
+			}
+		},
+	)
+}
+
+func racePullRepos(t *goRoutineTester, nodes []nodeNrType, suffix string, forBranches bool) {
+	runRace(
+		t,
+		nodes,
+		forBranches,
+		"Pulling: "+suffix,
+		func(t tester, nodes []nodeNrType, repo, branch int) {
+			reponame := fmt.Sprintf("race%d", repo)
+			dir := raceCloneDir(suffix, forBranches, repo, branch)
+			node := nthNode(nodes, repo, branch)
+			_clone(t, cloneMethodHTTPS, node, reponame, "admin", dir, true, false)
+		},
+	)
+}
+
+func runRace(t *goRoutineTester, nodes []nodeNrType, forBranches bool, descrip string, f func(t tester, nodes []nodeNrType, repo, branch int)) {
+	t.Logf(descrip)
+
+	var wg sync.WaitGroup
+	syncchan := make(chan interface{})
+
+	for i := 0; i < numRaceRepos; i++ {
+		for j := 0; j < numRaceBranches; j++ {
+			if forBranches || j == 0 {
+				wg.Add(1)
+				go func(repo, branch int) {
+					defer wg.Done()
+					// Wait until all routines are created
+					<-syncchan
+
+					// TODO: Run
+					f(t, nodes, repo, branch)
+				}(i, j)
+			}
 		}
 	}
-	t.Logf("Starting pushes")
+	t.Logf("Kicking of goroutines")
 	close(syncchan)
-	t.Logf("Waiting for pushes to finish")
-	pushWg.Wait()
-	t.Logf("Pushes finished")
-}
-
-func racePull(t tester, nodes []nodeNrType, postfix string) {
-	var pullWg sync.WaitGroup
-	syncchan := make(chan interface{})
-	t.Logf("Initializing pull %s", postfix)
-	for repo := 0; repo < numRaceRepos; repo++ {
-		reldir := fmt.Sprintf("race_%d_%s", repo, postfix)
-		dir := path.Join(cloneDir, reldir)
-		reponame := fmt.Sprintf("race%d", repo)
-		pullWg.Add(1)
-		go func(nr int, reponame, dir string) {
-			defer pullWg.Done()
-			node := nthNode(nodes, nr, 1)
-
-			<-syncchan
-
-			t.Logf("Pulling repo %s from node %d to %s", reponame, node, dir)
-			_clone(t, cloneMethodHTTPS, node, reponame, "admin", dir, true, false)
-		}(repo, reponame, dir)
-	}
-	t.Logf("Starting repo pulls")
-	close(syncchan)
-	t.Logf("Waiting for repo pulls to finish")
-	pullWg.Wait()
-	t.Logf("Repos pulled")
+	t.Logf("Waiting for finish")
+	wg.Wait()
+	t.Logf("Resolving fails")
+	t.resolveFails()
+	t.Logf("Done")
 }
