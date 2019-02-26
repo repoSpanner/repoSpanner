c0d99562e008c49626f4905e9d3b278285fd1f73
Patrick Uiterwijk <patrick@puiterwijk.org>
2019-02-27 00:17:02 +0100

    Migrate from dep to go mod
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- build.sh
+++ build.sh
@@ -1,40 +1,25 @@
 #!/bin/sh -xe
-if [ ! -d vendor/github.com ];
-then
-	dep ensure
-fi
-
-if [ "$FULLUPDATE" = "yes" ];
-then
-	dep ensure -v -update
-	git diff Gopkg.lock
-fi
-
 export VERSION="$(cat VERSION)"
 export GITDESCRIP="$(git describe --long --tags --dirty --always)"
-export VENDORDESCRIP="$(find vendor -type f | xargs cat | sha1sum | tr -d '\n' | sed -e 's/  -//')"
 
 (
     cd cmd/repospanner/
     go build -ldflags \
         "-X repospanner.org/repospanner/server/constants.version=$VERSION
-        -X repospanner.org/repospanner/server/constants.gitdescrip=$GITDESCRIP
-        -X repospanner.org/repospanner/server/constants.vendordescrip=$VENDORDESCRIP" \
+        -X repospanner.org/repospanner/server/constants.gitdescrip=$GITDESCRIP" \
         -o ../../repospanner $@
 )
 (
     cd cmd/repobridge/
     go build -ldflags \
         "-X repospanner.org/repospanner/server/constants.version=$VERSION
-        -X repospanner.org/repospanner/server/constants.gitdescrip=$GITDESCRIP
-        -X repospanner.org/repospanner/server/constants.vendordescrip=$VENDORDESCRIP" \
+        -X repospanner.org/repospanner/server/constants.gitdescrip=$GITDESCRIP" \
         -o ../../repobridge $@
 )
 (
     cd cmd/repohookrunner/
     go build -ldflags \
         "-X repospanner.org/repospanner/server/constants.version=$VERSION
-        -X repospanner.org/repospanner/server/constants.gitdescrip=$GITDESCRIP
-        -X repospanner.org/repospanner/server/constants.vendordescrip=$VENDORDESCRIP" \
+        -X repospanner.org/repospanner/server/constants.gitdescrip=$GITDESCRIP" \
         -o ../../repohookrunner $@
 )
---
+++ go.mod
@@ -0,0 +1,27 @@
+module repospanner.org/repospanner
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/coreos/etcd v3.3.12+incompatible
+	github.com/coreos/go-systemd v0.0.0-20181031085051-9002847aa142 // indirect
+	github.com/coreos/pkg v0.0.0-20180108230652-97fdf19511ea
+	github.com/gogo/protobuf v1.2.0 // indirect
+	github.com/golang/protobuf v1.2.0
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0
+	github.com/pkg/errors v0.8.1
+	github.com/prometheus/client_golang v0.9.2 // indirect
+	github.com/prometheus/client_model v0.0.0-20190129233127-fd36f4220a90 // indirect
+	github.com/prometheus/common v0.2.0 // indirect
+	github.com/prometheus/procfs v0.0.0-20190209105433-f8d8b3f739bd // indirect
+	github.com/sirupsen/logrus v1.3.0
+	github.com/spf13/afero v1.2.1 // indirect
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3
+	github.com/spf13/viper v1.3.1
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
+	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67 // indirect
+	golang.org/x/net v0.0.0-20181201002055-351d144fa1fc
+	golang.org/x/sys v0.0.0-20190209173611-3b5209105503 // indirect
+	golang.org/x/time v0.0.0-20181108054448-85acf8d2951c // indirect
+)
